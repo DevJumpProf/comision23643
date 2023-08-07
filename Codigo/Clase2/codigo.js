ed83ef59685cf05ff75665df2e1f9a770fb90923
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-08-07 20:41:19 -0300

    clase2

---
+++ Codigo/Clase2/codigo.js
@@ -0,0 +1,102 @@
+// interacciones
+
+/* alert ("Hola Codo a codo") 
+
+//prompt 
+
+prompt ("Decime tu nombre")
+
+/* confirm   */
+
+/*  confirm ("Bienvenid@ a mi pagina") */
+
+ // Caracteristicas
+/*  * interpretado 
+ * Orientado a Objetos
+ * imperativo
+ * Case sensitive = let nombreApellido = "Gustavo Ballas" -  let nombreapellido  = "Nicolino Locche"
+ * Basado en propotipos - instanacias - classless
+ * tipado debil
+ * lenguaje dinamico */
+
+// convenciones de nombre
+
+//camelCase (2 tipos)  - lo vamos a utilizar
+
+/* UpperCamelCase : la primer letra de cada palabra es mayuscula
+let NombreApellido
+
+lowerCamelCase: igual que la anterior con la excepcion de que la primera letra es en minuscula
+let nombreApellido
+ */
+
+
+/* snake_case
+mi_app
+MI_APP */
+
+/* train-case
+Mi-App */
+
+
+//variables
+
+/* var nombre = "Juana"
+let apellido = "Perez"
+
+const deporteFavorito = "Boxeo"
+document.write(deporteFavorito) */
+
+/* let nombre = prompt ("decime tu nombre")
+alert(nombre) */
+
+//tipos de datos
+/* let nombre = "Amilcar"; // cadena de texto - STRING
+let apellido = "Brusa";
+let edad = 70 // numero - NUMBER
+let tecnico = true // booleano - BOOLEAN
+let campeones = ["Oliveras", "Gallardo",true,32,"Carlos"] //arreglo -  ARRAY
+ */
+// concatenacion
+/* let nombre = prompt ("decime tu nombre")
+let num1 = Number(prompt ("decime un numero para sumar"))
+let num2 = Number(prompt ("decime otro numero para sumar"))
+let resultado = num1+num2
+
+/* "14"+"6"   */// concatenando 146
+
+/* document.write (nombre+" el resultado de tu suma es = " + resultado)
+  */
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
